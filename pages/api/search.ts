ee4a209e4403e0ab4f3bc11cea42131be8baf890
Dominik Ager <dominikager@Dominiks-MBP.home>
2022-04-09 20:07:43 +0200

    logic for searching facebook and tripadvisor link

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -3,19 +3,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import fetch from 'node-fetch'
 import cheerio from 'cheerio'
 
-type Result = {
-  url: string
-  name: string
-}
-
-type Data = {
-  results: Result[]
-  status?: {
-    code: number
-    message: string
-  }
-}
-
 function cors (res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
@@ -23,49 +10,42 @@ function cors (res: NextApiResponse) {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
 }
 
+async function searchForLink(q: string, platform: string) {
+  const url = "https://de.search.yahoo.com/search?q=" + q + " " + platform;
+  const optionsRes = await fetch(url, { method: 'GET' }).catch(() => undefined)
+
+  if (!optionsRes) {
+    return
+  }
+
+  const targetRes = await fetch(url)
+  const html = await targetRes.text().catch(() => '')
+ 
+  const $ = cheerio.load(html)
+  
+  return $('#web .first .dd.algo').map((i, card) => {
+    const response =  {
+      url: $(card).find("a").attr('href'),
+    }
+
+    return response;
+  }).get()
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-//  cors(res)
-  const { q, ...scrapes } = req.query
-
+  cors(res)
+  const { q } = req.query
+    
   if (typeof q !== 'string') {
     res.status(400).json({ error: 'query is required' })
     return
   }
 
-  const url = "https://de.search.yahoo.com/search?q=" + q;
+  const tripadvisor = await searchForLink(q, "Tripadvisor");
+  const facebook = await searchForLink(q, "Facebook");
 
-  const optionsRes = await fetch(url, { method: 'GET' }).catch(() => undefined)
-  if (!optionsRes) {
-    res.status(400).json({ error: 'could not load url' })
-    return
-  }
-
-  const status = {
-    code: optionsRes.status,
-    message: optionsRes.statusText,
-  }
-
-  const targetRes = await fetch(url)
-  const html = await targetRes.text().catch(() => '')
-  
-  if (!html) {
-    res.status(400).json({ error: 'could not url contents' })
-    return
-  }
-
-  const $ = cheerio.load(html)
-
-  const results = $('#web .first .dd.richAlgo').map((i, card) => {
-    const response =  {
-      url: $(card).find("a").attr('href'),
-      name: $(card).find('a').text(),
-    }
-
-    return response;
-  }).get()
-
-  res.json({ results,  status })
+  res.json({ tripadvisor, facebook })
 }
